0bdbb83714de40f696b892088661cbb8e1b945a7
Nethra Yamala <150650757+pixawesome@users.noreply.github.com>
2024-12-10 17:50:10 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ from tkinter.messagebox import *
 
 class FileEditor(): 
 
-    filename = None
+    filename = "Untitled"
 
     def __init__(self):
         self.root = Tk()
@@ -20,15 +20,8 @@ class FileEditor():
         self.text = Text(self.root, width = 400, height = 400)
 
     def new_file(self): 
-        self.filename = "Untitled"
         # 0.0 position = 0th line.0th column
         self.text.delete(0.0, END) # deletes all the charaters from the first character to the end
-
-    def save_file(self):
-        contents = self.text.get(0.0, END)
-        file = open(self.filename, "w")
-        file.write(contents)
-        file.close()
 
     def save_as(self):
         file = asksaveasfile(mode = "w", defaultextension=".txt")
@@ -50,7 +43,6 @@ class FileEditor():
         menubar = Menu(self.root)
         filebar = Menu(menubar)
         filebar.add_command(label= "New", command= self.new_file)
-        filebar.add_command(label= "Save", command= self.save_file)
         filebar.add_command(label= "Save As..", command= self.save_as)
         filebar.add_command(label= "Open", command= self.open_file)
         filebar.add_separator()
